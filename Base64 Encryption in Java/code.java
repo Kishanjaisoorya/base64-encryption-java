b7f58c2ec8dee3b3e38d1acabd17fa07f54a111c
Kishanjaisoorya <119406498+Kishanjaisoorya@users.noreply.github.com>
2023-09-17 19:26:36 +0530

    initial commit

--- Base64 Encryption in Java/code.java
+++ Base64 Encryption in Java/code.java
@@ -1,17 +1,23 @@
 import java.util.Base64;
+import java.util.Scanner;
 
 public class Base64EncryptionExample {
     public static void main(String[] args) {
-        String originalText = "Hello, Base64 Encryption!";
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the text to encode: ");
+        String originalText = scanner.nextLine();
+
         // Encoding (Encryption)
         byte[] encodedBytes = Base64.getEncoder().encode(originalText.getBytes());
         String encodedText = new String(encodedBytes);
         System.out.println("Encoded Text: " + encodedText);
-        
+
         // Decoding (Decryption)
         byte[] decodedBytes = Base64.getDecoder().decode(encodedBytes);
         String decodedText = new String(decodedBytes);
         System.out.println("Decoded Text: " + decodedText);
+
+        scanner.close();
     }
 }
